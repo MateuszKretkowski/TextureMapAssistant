9788954d770a1db0abb040971805c423d6f81ebf
Mateusz Kretkowski <mkretkowski13@gmail.com>
2024-12-08 18:16:50 +0100

    Major Bug Fixes (Whole Website is Responsive, other changes which improve website)

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -9,18 +9,18 @@ export const NavBar = () => {
             <div className="button_list">
                 <button>Color Duplicate Picker</button>
             </div>
-            <div className='title_container'>
-                <div className='title-left-side'>
-                    <div className="title_left-side_word_big">
+            <div className='title_container' onClick={() => {nav("/")}}>
+                <div className='title-left-side' onClick={() => {nav("/")}}>
+                    <div className="title_left-side_word_big" onClick={() => {nav("/")}}>
                         <h1>TEXTURE</h1>
                     </div>
-                    <div className='title_left-side_word'>
+                    <div className='title_left-side_word' onClick={() => {nav("/")}}>
                         <h1>M</h1>
                         <h1>A</h1>
                         <h1>P</h1>
                     </div>
                 </div>
-                <div className="title_right-side_word">
+                <div className="title_right-side_word" onClick={() => {nav("/")}}>
                     <h1>ASSISTANT</h1>
                 </div>
             </div>
